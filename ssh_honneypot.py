976fc40dce47b1e89e1369a94b24a3967708f962
Silver595 <akashpurjalkar66@gmail.com>
2025-07-21 02:22:40 +0530

    logger added

--- ssh_honneypot.py
+++ ssh_honneypot.py
@@ -1,3 +1,10 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-funnal_logger = logging.getLogger('funnel_logger')
+logging_format = logging.Formatter('%(message)s')
+
+funnal_logger = logging.getLogger('funnelLogger')
+funnal_logger.setLevel(logging.INFO)
+funnal_handler = RotatingFileHandler('audits.log',maxBytes=2000,backupCount=5)
+funnal_handler.setFormatter(logging_format)
+funnal_logger.addHandler(funnal_handler)
